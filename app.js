123691c6d90b729c9d24bf43375547aea18da8bd
krish736 <112505529+krish736@users.noreply.github.com>
2024-05-16 22:49:18 +0530

    commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('express start')
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./extra_html/index.html", "utf-8");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
